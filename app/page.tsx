5410cb75585cac8e4f86fadfc7ab3d95c7736209
Maryam <wiut13789@gmail.com>
2024-10-21 20:12:49 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-6xl text-center">Auto Deal</h1>
+      <Button>Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="outline">Click me</Button>
+    </div>
+  );
+}
